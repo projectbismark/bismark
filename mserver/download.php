9683f85b263fe7cf774f949273b2b0f33314e4db
wdd <walter.dedonato@unina.it>
2010-05-20 19:56:53 +0000

    download.php mbps parameter
    git-svn-id: https://svn.comics.unina.it/bismark/trunk@99 e00a4238-d597-4720-b6f7-de6324f10642

--- mserver/download.php
+++ mserver/download.php
@@ -1,16 +1,38 @@
 <?php
 	// Check duration
 	$duration = $_GET['duration'];
-	if ($duration == 0 || $duration >= 60)
-		$duration=10;
+	if ($duration <= 1)
+		$duration = 2;
+	if ($duration >= 60)
+		$duration = 60;
 	
+	// Check size
+	$rate = $_GET['mbps'];
+	if ($rate <= 12)
+		$size = 1;
+	else if ($rate <= 50)
+		$size = intval(($rate / 8) + 1);
+	else if ($rate <= 128)
+		$size = intval($rate / 6.4);
+	else if ($rate <= 256)
+		$size = intval(($rate / 6.4) + ($rate/256));
+	else if ($rate <= 512)
+		$size = intval(($rate / 5.9) + ($rate/256)^2);
+
+
+
 	// Set time variables
 	$start = time();
 	$end = $start + $duration;
 
+	// Create payload pattern
+	$str = "";
+	for ($i=0;$i<=$size;$i++)
+		$str = sprintf("%s%c", $str, (48 + $i) % 78);
+
 	// Generate payload 
 	do { 
 		$countdown = $end - time(); 
-		printf("|%u|", $countdown);
+		printf("%s", $str);
 	} while($countdown > 0);
 ?>
