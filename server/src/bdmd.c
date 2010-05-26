db97302f337e09dec3839b300b7fd8e218058710
wdd <walter.dedonato@unina.it>
2010-05-26 23:55:55 +0000

    bdmd C multi-threaded beta2
    git-svn-id: https://svn.comics.unina.it/bismark/trunk@105 e00a4238-d597-4720-b6f7-de6324f10642

--- server/src/bdmd.c
+++ server/src/bdmd.c
@@ -1,226 +1,336 @@
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// UDP Proxy Server multi-thread
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+/*
+ * Bismark Device Manager Daemon
+ *
+ * Created on: 25/05/2010
+ * Author: walter.dedonato@unina.it
+ */
 
-// Librerie
+/*
+ * Dependencies
+ */
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>	// gethostbyname()
+#include <netdb.h>
 #include <sys/time.h>
 #include <time.h>
-
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> 	// atoi()
-#include <unistd.h> 	// close()
-
-#include <pthread.h>	// posix threads
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 #include <sqlite3.h>
 
-// Costanti
-#define QLEN 6
-#define MAX_NUM_THREAD 50
+/*
+ * Types
+ */
+/* Thread parameter */
+typedef struct {
+	struct sockaddr_in cad; /* Client-side socket info */
+	char *payload; 		/* Received payload */
+} thp;
 
-// Variabili globali (condivise da tutti i thread)
-int num_thread;			// numero di thread attivi
-char gbuf[1000];		// buffer in cui memorizzare la stringa
-struct sockaddr_in sad;		// SERVER
+/* Probe format */
+typedef struct {
+	char *id; 	/* Device id */
+	char *cmd; 	/* Command string */
+	char *param; 	/* Parameter string */
+} pf;
+
+/* Message row */
+typedef struct {
+	char *mid; 	/* Message id */
+	char *from; 	/* Sender id */
+	char *to; 	/* Receiver id */
+	char *msg; 	/* Message string */
+} mrowf;
+
+/*
+ * Constants
+ */
+#define BDM_DB "/var/tmp/bismark/db/bdm.db"
+#define LOG_DIR "/var/tmp/bismark/log/devices/"
+
+#define MAX_NUM_THREAD 50
+#define MAX_UDP_PSIZE 1472
+#define MAX_QUERY_LEN 1000
+#define MAX_IP_LEN 16
+#define MAX_FILENAME_LEN 50
+
+/*
+ * Globals
+ */
+int num_thread; 	/* Threads counter */
+struct sockaddr_in sad; /* Server-side socket info */
+int ssd; 		/* Server socket descriptor */
+
+/* Mutex */
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_db = PTHREAD_MUTEX_INITIALIZER;
-int gsd;
 
+/*
+ * Query callback function
+ */
 static int callback(void *out, int argc, char **argv, char **azColName)
 {
-	if (argc == 1)
-		strncpy(out, argv[0], strlen(argv[0]));
-	else if (argc > 1) {
-		int i, p = 0;
+	char *tmp;
 
-		for (i=0; i<argc-1; i++) {
-			snprintf(&((char *)out)[p], 64 - p, "%s ", argv[i]);
-			p = strlen(out);
+	/* Parse row */
+	if (argc == 1) {
+		/* One column */
+		tmp = calloc(1, strlen(argv[0]) + 1);
+		strncpy(tmp, argv[0], strlen(argv[0]));
+	} else if (argc > 1) {
+		/* More columns */
+		int i, p, len;
+
+		/* Compute row len */
+		for (i = 0, len = 0; i < argc; i++) {
+			len += strlen(argv[i]) + 1;
 		}
-		snprintf(&((char *)out)[p], 64 - p, "%s", argv[i]);
-	} else
-		((char *)out)[0] = '\0';
+
+		/* Create output string */
+		tmp = calloc(1, len);
+		for (i = 0, p = 0; i < argc - 1; i++) {
+			snprintf(&tmp[p], (len - p), "%s ", argv[i]);
+			p = strlen(tmp);
+		}
+		snprintf(&tmp[p], (len - p), "%s", argv[i]);
+	} else {
+		/* No result */
+		tmp = NULL;
+	}
+
+	/* Set output */
+	*(char **)out = tmp;
+
 	return 0;
 }
 
-char *query_exec(sqlite3 *db, const char *query, const int cb)
+/*
+ * Execute a query (thread safe)
+ */
+char *do_query(sqlite3 *db, const char *query, const int cb)
 {
-	char *out = calloc(1, 64);
-	char *zErrMsg = 0;
+	char *out = NULL, *err = 0;
 
 	pthread_mutex_lock(&mutex_db);
-	printf("query = %s\n", query);
-	if (sqlite3_exec(db, query, cb ? callback : NULL, out, &zErrMsg) != SQLITE_OK) {
-		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
+	if (sqlite3_exec(db, query, cb ? callback : NULL, &out, &err) != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", err);
+		sqlite3_free(err);
 	}
-	printf("out = %s\n", out);
 	pthread_mutex_unlock(&mutex_db);
-
 
 	return out;
 }
 
-// Thread per la gestione di una singola connessione
-void *doit(void *vncad)
+/*
+ * Probe handler thread
+ */
+void *doit(void *param)
 {
-	char lbuf[1000];			// buffer per la stringa modificata
-	char query[1000];
+	thp *tp = (thp *) param; 	/* Thread parameters */
+	char query[MAX_QUERY_LEN]; 	/* Query string buffer */
+	time_t ts; 			/* Current timestamp */
+	pf probe; 			/* Probe dissection */
+	char ip[MAX_IP_LEN]; 		/* Client IP address */
+	sqlite3 *db; 			/* DB handler */
+	int thread_id; 			/* Thread identifier */
+	char *reply = NULL; 		/* Reply message */
+	char date[25]; 			/* Date string */
 	int i;
-	struct timeval timeout = {2,0};
-	time_t ts;
-	char *tmp;
-	char id[10], cmd[15], param[50], ip[16];
-	sqlite3 *db;
-	
+
+	/* Get thread identifier */
 	pthread_mutex_lock(&mutex);
-	struct sockaddr_in ncad = *((struct sockaddr_in *)vncad);	// copia locale del descrittore della socket
-	int thread_id = num_thread++;					// identificativo logico del thread
-	strncpy(lbuf, gbuf, 1000);
-	tmp = (char *)inet_ntoa(ncad.sin_addr);
-	strncpy(ip, tmp, 16);
+	thread_id = num_thread++;
 	pthread_mutex_unlock(&mutex);
 
-	// Get time
+	/* Get client IP address */
+	strncpy(ip, (const char *) inet_ntoa(tp->cad.sin_addr), 16);
+
+	/* Get timestamp */
 	ts = time(NULL);
+	strftime(date, sizeof(date), "%Y/%m/%d %H:%M:%S", localtime(&ts));
 
-	// Parse probe packet payload
-	sscanf(lbuf,"%s %s %s", id, cmd, param);
+	/* Parse probe packet payload */
+	probe.id = tp->payload;
+	for (i = 0; tp->payload[i] != ' '; i++)	;
+	probe.cmd = &tp->payload[i + 1];
+	for (tp->payload[i] = 0; tp->payload[i] != ' '; i++);
+	probe.param = &tp->payload[i + 1];
+	for (tp->payload[i] = 0; tp->payload[i] != '\n'; i++);
+	tp->payload[i] = 0;
 
-	// Set default reply
-	sprintf(lbuf,"pong %s\n", ip);
+	/* Output log entry */
+	printf("%s - \"%s %s\" from %s [%s]\n", date, probe.cmd, probe.param, probe.id, ip);
 
-	// Exec command
-	if (!strncmp(cmd, "ping", 4)) {
-		// Ping
-		char *val = NULL;
-
-		// Open sqlite db
-		if (sqlite3_open("/var/tmp/bdm.db", &db)) {
-			fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-			sqlite3_close(db);
-			return NULL;
-		}
-
-		// Check device presence in db
-		snprintf(query, 1000, "SELECT id FROM devices WHERE id='%s';", id);
-		val = query_exec(db, query, 1);
-
-		// Update db
-		printf ("Th %d: ", thread_id);
-		if (*val) {
-			free(val);
-			snprintf(query, 1000, "UPDATE devices SET ip='%s',ts=%lu,version=%s WHERE id='%s';", ip, ts, param, id);
-			query_exec(db, query, 0);
-		} else {
-			snprintf(query, 1000, "INSERT INTO devices (id, ip, ts, version) VALUES('%s','%s',%lu,'%s');", id, ip, ts, param);
-			query_exec(db, query, 0);
-		}
-
-		// Check messages
-		printf ("Th %d: ", thread_id);
-		snprintf(query, 1000, "SELECT rowid,* FROM messages WHERE \"to\"='%s' LIMIT 1;", id);
-		val = query_exec(db, query, 1);
-		if (*val) {
-			char mid[5], from[10], to[10], msg[10];
-
-			// Parse query result
-			sscanf(val,"%s %s %s %s", mid, from, to, msg);
-
-			// Set reply message
-			sprintf(lbuf,"%s\n", msg);
-
-			printf ("Th %d: ", thread_id);
-			snprintf(query, 1000, "DELETE FROM messages WHERE rowid='%s';", mid);
-			query_exec(db, query, 0);
-		}
-
+	/* Open db */
+	if (sqlite3_open(BDM_DB, &db)) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
 		sqlite3_close(db);
-	} else if (!strncmp(cmd, "log", 3)) {
-		// Log
-		snprintf(query, 1000, "INSERT INTO messages ('from', 'to', msg) VALUES('%s','BDM','%s');", id, param);
-		printf("%s\n",query);
+		return NULL;
 	}
 
-	// Invio della risposta del server al client
-	sendto(gsd, lbuf, strlen(lbuf),0,(struct sockaddr*) &ncad, sizeof(ncad));
-	
+	/* Parse command */
+	if (!strncmp(probe.cmd, "ping", 4)) {
+		/* Ping */
+		char *row; 	/* Query result row */
+
+		/* Check device presence in db */
+		snprintf(query, MAX_QUERY_LEN, "SELECT id FROM devices WHERE id='%s';", probe.id);
+		if (do_query(db, query, 1)) {
+			/* Update db entry */
+			snprintf(query, MAX_QUERY_LEN, "UPDATE devices SET ip='%s',ts=%lu,version=%s WHERE id='%s';", ip, ts, probe.param,
+				probe.id);
+			do_query(db, query, 0);
+		} else {
+			/* Insert new db entry */
+			snprintf(query, MAX_QUERY_LEN, "INSERT INTO devices (id, ip, ts, version) VALUES('%s','%s',%lu,'%s');", probe.id,
+				ip, ts, probe.param);
+			do_query(db, query, 0);
+		}
+
+		/* Check messages */
+		snprintf(query, MAX_QUERY_LEN, "SELECT rowid,* FROM messages WHERE \"to\"='%s' LIMIT 1;", probe.id);
+		if ((row = do_query(db, query, 1))) {
+			mrowf msg; 	/* Message row fields */
+
+			/* Parse query result */
+			msg.mid = row;
+			for (i = 0; row[i] != ' '; i++);
+			msg.from = &row[i + 1];
+			for (row[i] = 0; row[i] != ' '; i++);
+			msg.to = &row[i + 1];
+			for (row[i] = 0; row[i] != ' '; i++);
+			msg.msg = &row[i + 1];
+			row[i] = 0;
+
+			/* Set reply message */
+			reply = malloc(strlen(msg.msg) + 2);
+			sprintf(reply, "%s\n", msg.msg);
+
+			/* Output log entry */
+			printf("%s - Delivered message from %s to %s: %s\n", date, msg.from, msg.to, msg.msg);
+
+			/* Remove message from db */
+			snprintf(query, MAX_QUERY_LEN, "DELETE FROM messages WHERE rowid='%s';", msg.mid);
+			do_query(db, query, 0);
+			free(row);
+		} else {
+			/* Set pong reply */
+			reply = malloc(MAX_IP_LEN + 7);
+			sprintf(reply, "pong %s\n", ip);
+		}
+	} else if (!strncmp(probe.cmd, "log", 3)) {
+		/* Log */
+		FILE *lfp;			/* Log file pointer */
+		char logfile[MAX_FILENAME_LEN];	/* Log file name */
+		char *log = &tp->payload[i + 1];	/* Log data in current packet */
+
+		/* Append log to logfile */
+		snprintf(logfile, MAX_FILENAME_LEN, "%s/%s.log", LOG_DIR, probe.id);
+		lfp = fopen(logfile, "a");
+		fprintf(lfp, "%s - %s\n%s\n", date, probe.param, log);
+		fclose(lfp);
+
+		/* Send message to bdm client */
+		snprintf(query, MAX_QUERY_LEN, "INSERT INTO messages ('from', 'to', msg) VALUES('%s','BDM','%s');", probe.id, probe.param);
+		do_query(db, query, 0);
+	}
+
+	/* Close db */
+	sqlite3_close(db);
+
+	/* Send reply to client */
+	if (reply) {
+		sendto(ssd, reply, strlen(reply), 0, (struct sockaddr*) &tp->cad, sizeof(tp->cad));
+		free(reply);
+	}
+	free(tp->payload);
+	free(tp);
+
+	/* Update threads counter */
 	pthread_mutex_lock(&mutex);
 	num_thread--;
 	pthread_mutex_unlock(&mutex);
-	
+
 	return NULL;
 }
 
-
+/*
+ * Entry point
+ */
 int main(int argc, char *argv[])
 {
-	unsigned short server_port;	// numero di port del servizio
-	struct hostent *ptrh;		// struttura per memorizzare la risoluzione del nome del server
-	pthread_t hThr[MAX_NUM_THREAD];
+	unsigned short server_port; 	/* Server port number */
+	pthread_t hThr[MAX_NUM_THREAD]; /* Threads handlers */
 	int i = 0;
 
-	// Lettura delle opzioni a riga di comando
+	/* Command-line check */
 	if (argc != 2) {
 		fprintf(stderr, "usage: %s <port>\n", argv[0]);
 		return 0;
 	}
 
-	// Acquisizione port del server
+	/* Port number parsing */
 	server_port = atoi(argv[1]);
-	if (server_port == 0) {
+	if (server_port < 0 || server_port > 65534) {
 		fprintf(stderr, "invalid port number: %s\n", argv[1]);
 		return 1;
 	}
 
-	// Inzializzazione della struttura sad (Server)
-	memset((char *)&sad, 0, sizeof(sad));
+	/* Server-side socket info initialization */
+	memset((char *) &sad, 0, sizeof(sad));
 	sad.sin_family = AF_INET;
 	sad.sin_addr.s_addr = htonl(INADDR_ANY);
 	sad.sin_port = htons(server_port);
 
-	// Creazione di una socket di tipo SOCK_DGRAM - UDP
-	if ((gsd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+	/* Socket creation */
+	if ((ssd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
 		fprintf(stderr, "error creating socket\n");
 		return 1;
 	}
 
-	// Configurazione dell'endpoint locale della socket utilizzando 
-	// i parametri della struttura sockaddr_in sad
-	if (bind(gsd, (struct sockaddr *)&sad, sizeof(sad)) < 0) {
+	/* Socket binding */
+	if (bind(ssd, (struct sockaddr *) &sad, sizeof(sad)) < 0) {
 		fprintf(stderr, "bind fallito\n");
 		return 1;
 	}
 
 	printf("Bdmd listening on port %d\n", server_port);
 
-	// Ciclo infinito
+	/* Infinite loop */
 	while (1) {
-		char buf[1000];					// buffer in cui memorizzare la stringa
-		struct sockaddr_in cad;				// CLIENT
-		socklen_t cadlen = sizeof(struct sockaddr_in);	// dimensione della struttura cad
-		
-		// Ricezione della stringa ricevuta dal client
-		memset(buf, 0, sizeof(buf));
-		recvfrom(gsd, buf, sizeof(buf), 0, (struct sockaddr*)&cad, &cadlen);
-		pthread_mutex_lock(&mutex);
-		strncpy(gbuf, buf, 1000);
-		pthread_mutex_unlock(&mutex);
-		
-		// Controllo sul numero massimo di socket gestibili
+		char payload[MAX_UDP_PSIZE];	/* Temporary buffer to store payload */
+		struct sockaddr_in cad;		/* Temporary Client-side socket info */
+		socklen_t cadlen;		/* Client-side socket info length */
+		thp *ntp;			/* New thread parameters */
+		int bytes;
+
+		/* Listen for probe packets */
+		cadlen = sizeof(struct sockaddr_in);
+		bytes = recvfrom(ssd, payload, MAX_UDP_PSIZE, 0, (struct sockaddr*) &cad, &cadlen);
+		if (bytes <= 0) {
+			fprintf(stderr, "error receiving probe packet\n");
+			continue;
+		}
+
+		/* Prepare thread parameters */
+		ntp = malloc(sizeof(thp));
+		ntp->cad = cad;
+		ntp->payload = calloc(1, bytes + 1);
+		strncpy(ntp->payload, payload, bytes);
+
+		/* Check max thread number */
 		if (num_thread >= MAX_NUM_THREAD) {
-			fprintf(stderr, "max connections reached: refusing %s\n", buf);
+			fprintf(stderr, "max connections reached: refusing connection from %s\n", (char *) inet_ntoa(cad.sin_addr));
 		} else {
-			// Creazione di un thread per gestire la nuova connessione
-			if (pthread_create(&hThr[i], NULL, doit, &cad) < 0) {
+			/* Create thread to handle the connection */
+			if (pthread_create(&hThr[i], NULL, doit, ntp) < 0) {
 				fprintf(stderr, "error creating thread\n");
 			}
 		}
 	}
-	
-	// Chiusura della socket in ascolto
- 	close(gsd);
+
+	/* Close socket */
+	close(ssd);
 }
