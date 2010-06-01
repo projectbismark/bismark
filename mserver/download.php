4a0ba22c16cdebad6046b9c9d9578c43a9cf9120
wdd <walter.dedonato@unina.it>
2010-06-01 22:12:59 +0000

    better download.php
    git-svn-id: https://svn.comics.unina.it/bismark/trunk@113 e00a4238-d597-4720-b6f7-de6324f10642

--- mserver/download.php
+++ mserver/download.php
@@ -6,20 +6,10 @@
 	if ($duration >= 60)
 		$duration = 60;
 	
-	// Check size
-	$rate = $_GET['mbps'];
-	if ($rate <= 12)
-		$size = 1;
-	else if ($rate <= 50)
-		$size = intval(($rate / 8) + 1);
-	else if ($rate <= 128)
-		$size = intval($rate / 6.4);
-	else if ($rate <= 256)
-		$size = intval(($rate / 6.4) + ($rate/256));
-	else if ($rate <= 512)
-		$size = intval(($rate / 5.9) + ($rate/256)^2);
-
-
+	// Compute max transfer size
+	$rate = $_GET['kbps'] / 8;
+	$bytes = $rate * 1000 * $duration;
+	$size = 1460;
 
 	// Set time variables
 	$start = time();
@@ -30,9 +20,11 @@
 	for ($i=0;$i<=$size;$i++)
 		$str = sprintf("%s%c", $str, (48 + $i) % 78);
 
-	// Generate payload 
+	// Generate payload
+	$bytes_left = $bytes;
 	do { 
 		$countdown = $end - time(); 
+		$bytes_left -= $size; 
 		printf("%s", $str);
-	} while($countdown > 0);
+	} while($countdown > 0 && $bytes_left > 0);
 ?>
