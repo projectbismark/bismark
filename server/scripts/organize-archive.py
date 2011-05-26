9744d42d45822f6e170780246a093111a677348b
feamster <feamster@gatech.edu>
2011-05-26 19:01:18 +0000

    organization script is now functional, publishing to the web every two hours.
    currently only running for NB105.  Other devices to come.
    
    
    
    git-svn-id: https://svn.comics.unina.it/bismark/trunk@351 e00a4238-d597-4720-b6f7-de6324f10642

--- server/scripts/organize-archive.py
+++ server/scripts/organize-archive.py
@@ -8,11 +8,9 @@ import datetime
 import shutil
 import tarfile
 
-def clean_data(dir,targetdir):
 
-    UKY_DIR = os.environ['HOME'] + '/var/archive/UKY-old'
-    devices = []
-    measurements = []
+def filename_to_dir(file):
+
     types = {'arp.gz':'arp',
              'csv':'airodump',
              'csv.gz':'airodump',
@@ -20,12 +18,47 @@ def clean_data(dir,targetdir):
              'filt.csv':'airodump',
              'xml.gz':'active'}
 
+    # get device name
+    match = re.search(r'[0-9A-Za-z]+',file)
+    if match:
+        dev_name = match.group()
+    else:
+        return ''
+            
+    # get date
+    match = re.search(r'([0-9]{9,10})',file)
+    if match:
+        timestr = match.group(1)
+        date = datetime.date.fromtimestamp(float(timestr))
+        datedir = str(date.year) + '/' + str(date.month)
+    else:
+        return ''
+
+
+    # get type
+    match = re.search(r'.*?[0-9]\.(.*)$',file)
+    if match:
+        extension = match.group(1)
+        typedir = types[extension]
+    else:
+        return ''
+
+
+    return dev_name + '/' + typedir + '/'+ datedir
+
+
+
+def clean_data(dir,targetdir):
+
+    UKY_DIR = os.environ['HOME'] + '/var/archive/UKY-old'
+    devices = []
+    measurements = []
 
     files = os.listdir(dir)
     for file in files:
 
         if os.path.isdir(dir+file):
-            print 'skipping ' + file
+            print 'skipping ' + dir+file
             continue
 
 
@@ -35,29 +68,8 @@ def clean_data(dir,targetdir):
             shutil.move(dir+file, UKY_DIR)
             continue
 
-        # get list of devices
-        match = re.search(r'[0-9A-Za-z]+',file)
-        if match:
-            dev_name = match.group()
-            
-            # list of devices
-            if dev_name not in devices:
-                devices.append(dev_name)
-
-        match = re.search(r'([0-9]{10})',file)
-        if match:
-            timestr = match.group(1)
-            date = datetime.date.fromtimestamp(float(timestr))
-            datedir = str(date.year) + '/' + str(date.month)
-
-        match = re.search(r'.*?[0-9]\.(.*)$',file)
-        if match:
-            extension = match.group(1)
-            typedir = types[extension]
-
-
         # make directories and move files
-        datadir = targetdir + dev_name + '/' + typedir + '/'+ datedir
+        datadir = targetdir + filename_to_dir(file)
 
         if not os.path.exists(datadir):
             os.makedirs(datadir)
@@ -69,28 +81,47 @@ def clean_data(dir,targetdir):
     for dev in devices:
         print dev
 
-def new_device_files(members,device,dir):
+def new_device_files(members,device,pubdir):
     for tarinfo in members:
         match = re.search(device,tarinfo.name)
-        if match:
+        tfile = pubdir + filename_to_dir(tarinfo.name) + '/' + tarinfo.name
+        #if match:
+        #    print tfile
+        if match and not os.path.exists(tfile):
             yield tarinfo
         
 
-def unpack_backup(device,dir,outdir):
+def unpack_backup(device,dir,tmpdir,pubdir):
 
+    # get the tarfiles in the archive
     files = os.listdir(dir)
     for file in files:
+        
+        # make a directory to unpack the tarfile
+        try: 
+            os.mkdir(tmpdir)
+        except OSError:
+            pass
 
+        # look for the active measurement files
         match = re.search(r'xml.tgz',file)
         if match:
             # unpack into the unpack dir
             print file
             try:
                 tar = tarfile.open(dir+file,'r:gz')
-                tar.extractall(outdir,members=new_device_files(tar,device))
+
+                # unpack all device files that aren't yet published into the unpack dir
+                tar.extractall(tmpdir,members=new_device_files(tar,device,pubdir))
                 tar.close()
             except tarfile.ReadError:
                 print "Warning Read Error"
+
+        # publish the data with the correct directory structure
+        clean_data(tmpdir,pubdir)
+        
+        # remove the temporary dir
+        shutil.rmtree(tmpdir)
 
 
 if __name__ == '__main__':
@@ -99,12 +130,9 @@ if __name__ == '__main__':
 
     ARCHIVE_DIR = HOME + MEASURE_FILE_DIR + 'old/'
     BACKUP_DIR = HOME + 'var/backup/'
-    UNPACK_DIR = ARCHIVE_DIR + 'unpack/'
+    UNPACK_DIR = '/data/bismark/unpack/'
 
-    PUBLISH_DIR = ARCHIVE_DIR
-
-    clean_data(ARCHIVE_DIR,PUBLISH_DIR)
+    PUBLISH_DIR = '/data/bismark/public/'
 
     # restore directory structure from backup
-    #unpack_backup('NB105',BACKUP_DIR,UNPACK_DIR)    
-    #clean_data(UNPACK_DIR,PUBLISH_DIR)
+    unpack_backup('NB105',BACKUP_DIR,UNPACK_DIR,PUBLISH_DIR)    
