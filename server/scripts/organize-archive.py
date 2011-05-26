623cfe22bda89f498572c0973d6a0ec4b0599791
feamster <feamster@gatech.edu>
2011-05-26 08:22:51 +0000

    functions for publishing files from the archive
    git-svn-id: https://svn.comics.unina.it/bismark/trunk@348 e00a4238-d597-4720-b6f7-de6324f10642

--- server/scripts/organize-archive.py
+++ server/scripts/organize-archive.py
@@ -6,8 +6,9 @@ import sys
 import time
 import datetime
 import shutil
+import tarfile
 
-def clean_data(dir):
+def clean_data(dir,targetdir):
 
     UKY_DIR = os.environ['HOME'] + '/var/archive/UKY-old'
     devices = []
@@ -32,6 +33,7 @@ def clean_data(dir):
         match = re.search(r'[Uu]ky',file)
         if match:
             shutil.move(dir+file, UKY_DIR)
+            continue
 
         # get list of devices
         match = re.search(r'[0-9A-Za-z]+',file)
@@ -55,25 +57,54 @@ def clean_data(dir):
 
 
         # make directories and move files
-        datadir = dir+ dev_name + '/' + typedir + '/'+ datedir
+        datadir = targetdir + dev_name + '/' + typedir + '/'+ datedir
 
         if not os.path.exists(datadir):
             os.makedirs(datadir)
 
-        #print dir+file + '->' + datadir
+        print dir+file + '->' + datadir
         shutil.move(dir+file,datadir)
 
 
     for dev in devices:
         print dev
+
+def new_device_files(members,device,dir):
+    for tarinfo in members:
+        match = re.search(device,tarinfo.name)
+        if match:
+            yield tarinfo
         
 
-            
+def unpack_backup(device,dir,outdir):
+
+    files = os.listdir(dir)
+    for file in files:
+
+        match = re.search(r'xml.tgz',file)
+        if match:
+            # unpack into the unpack dir
+            print file
+            try:
+                tar = tarfile.open(dir+file,'r:gz')
+                tar.extractall(outdir,members=new_device_files(tar,device))
+                tar.close()
+            except tarfile.ReadError:
+                print "Warning Read Error"
 
 
 if __name__ == '__main__':
     HOME = os.environ['HOME'] + '/'
     MEASURE_FILE_DIR = 'var/data/'
-    ARCHIVE_DIR = HOME + MEASURE_FILE_DIR + 'old/'
 
-    clean_data(ARCHIVE_DIR)
+    ARCHIVE_DIR = HOME + MEASURE_FILE_DIR + 'old/'
+    BACKUP_DIR = HOME + 'var/backup/'
+    UNPACK_DIR = ARCHIVE_DIR + 'unpack/'
+
+    PUBLISH_DIR = ARCHIVE_DIR
+
+    clean_data(ARCHIVE_DIR,PUBLISH_DIR)
+
+    # restore directory structure from backup
+    #unpack_backup('NB105',BACKUP_DIR,UNPACK_DIR)    
+    #clean_data(UNPACK_DIR,PUBLISH_DIR)
