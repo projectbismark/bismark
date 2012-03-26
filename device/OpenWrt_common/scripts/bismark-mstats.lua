b3d952ec4ac8fb32185cf8831c01eb98ed0b7114
Stephen Woodrow <srwoodrow@gmail.com>
2012-03-25 22:39:35 -0400

    Add better docs, and include direction.

--- device/OpenWrt_common/scripts/bismark-mstats.lua
+++ device/OpenWrt_common/scripts/bismark-mstats.lua
@@ -9,13 +9,12 @@ require('math')
 
 MSTATS_STRICT_PARSE_ERROR = -1000
 
+-- Print usage information.
 function usage(me)
     print(string.format(
-            "USAGE: %s param toolname src_ip dst_ip exit_status [direction]",
-            me))
-    print("  where a string of real numbers, one per line, is provided to " ..
-          "standard input")
-    os.exit(2)
+[[USAGE: %s param toolname src_ip dst_ip exit_status [direction]
+  where a series of real numbers, one per line, is provided to standard input
+  to be summarized by %s]], me, me))
 end
 
 -- Parse number-per-line from standard input and output table and some
@@ -35,13 +34,11 @@ end
 --         sample variance of the elements in the table
 function parse_stdin(strict)
     if not strict then strict = true end  -- strict=true by default
+    data = {}
     count = 0
     mean = 0
-    -- sample variance computed by Welford's method
-    -- see Knuth's The Art of Computer Programming v.2, 2ed, page 232
     M2 = 0
     variance = nil
-    data = {}
 
     while true do
         line = io.read('*line')
@@ -50,10 +47,13 @@ function parse_stdin(strict)
         if element then
             data[#data+1] = element
             count = count + 1
+            -- sample variance computed by Welford's method
+            -- see Knuth's The Art of Computer Programming v.2, 2ed, page 232
             delta = element - mean
             mean = mean + delta/count
             M2 = M2 + delta*(element - mean)
         elseif strict then
+            -- we hit a bad row, and strict parsing is enabled -- return nil
             data = nil
             break
         end
@@ -72,6 +72,15 @@ function parse_stdin(strict)
     return data, count, mean, variance
 end
 
+-- Compute statistics on data and print XML measurement output. If no data is
+-- present, change exitstatus to value of MSTATS_STRICT_PARSE_ERROR.
+--
+-- kwargs   - a table containing attributes and their values to be included in
+--            the XML element.
+-- data     - a table containing data to be summarized.
+-- count    - the number of data items processed.
+-- mean     - the arithmetic mean of data.
+-- variance - the sample variance of data.
 function generate_output(kwargs, data, count, mean, variance)
     measure_attrs = {}
     for k,v in pairs(kwargs) do measure_attrs[k] = v end
@@ -95,9 +104,18 @@ function generate_output(kwargs, data, count, mean, variance)
     else
         measure_attrs['exitstatus'] = MSTATS_STRICT_PARSE_ERROR
     end
-    print_xml(measure_attrs)
+    print_measurement_xml(measure_attrs)
 end
 
+-- Compute the median of a sorted table.
+--
+-- t - table containing a sorted list of items
+--
+-- Returns the median value of the table,
+--         the lower index of the value used to compute the median,
+--         the upper index of the value used to compute the median.
+-- In the case that the table has an odd number of items, the lower and upper
+-- index contain the same value.
 function median(t)
     if #t % 2 == 0 then
         lindex = math.floor(#t/2)
@@ -111,7 +129,12 @@ function median(t)
     end
 end
 
-function print_xml(attrs)
+-- Print a measurement XML entity containing the attributes and values
+-- provided, ordered by attribute name.
+--
+-- attrs - a table containing attributes and values to be included in the XML
+--         element.
+function print_measurement_xml(attrs)
     io.write('    <measurement ')
     keys = {}
     for k in pairs(attrs) do
@@ -124,17 +147,21 @@ function print_xml(attrs)
     io.write('/>\n')
 end
 
+
 function main(argv)
     if #argv < 5 then
         usage(argv[0])
-    else
-        kwargs = {param=argv[1],
-                  tool=argv[2],
-                  srcip=argv[3],
-                  dstip=argv[4],
-                  exitstatus=argv[5]}
-        generate_output(kwargs, parse_stdin())
+        os.exit(2)
     end
+    kwargs = {param=argv[1],
+              tool=argv[2],
+              srcip=argv[3],
+              dstip=argv[4],
+              exitstatus=argv[5]}
+    if #argv == 6 then
+        kwargs['direction'] = argv[6]
+    end
+    generate_output(kwargs, parse_stdin())
 end
 
 return main(arg)
